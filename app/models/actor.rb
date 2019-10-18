ddd267cee4744fd1bbf71989648301079b2c00e0
Ronald Stephens <rstephens@bluemorpho.cc>
2019-10-18 19:11:53 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -7,7 +7,7 @@ class Actor < ActiveRecord::Base
   end
 
   def list_roles
-    self.character.map do |character|
+    self.characters.map do |character|
       "#{character.name} - #{character.show.name}"
     end
   end
