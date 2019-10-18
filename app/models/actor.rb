a29001c72fb17a19f6a0221bec10efa7f41958dd
Ronald Stephens <rstephens@bluemorpho.cc>
2019-10-18 18:36:57 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,13 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :shows
+  has_many :characters
+
+  def full_name
+    full_name = "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    self.shows
+    self.characters
+  end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,9 @@
 class Show < ActiveRecord::Base
-  
+  has_many :actors
+  has_many :characters
+
+  def actors_list
+    self.first_name
+    self.last_name
+  end
 end
